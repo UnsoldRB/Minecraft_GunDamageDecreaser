068ae140b666dadbfd5d0c0385ef0f260983e2e9
UnsoldRB <takuma2977@yahoo.co.jp>
2024-01-12 22:29:58 +0900

    最適化

--- src/main/java/com/unsoldriceball/gundamagedecreaser/Main.java
+++ src/main/java/com/unsoldriceball/gundamagedecreaser/Main.java
@@ -6,15 +6,16 @@ import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import org.jline.utils.Log;
+
 
 
 
 @Mod(modid = Main.MOD_ID, acceptableRemoteVersions = "*")
-public class Main {
-
+public class Main
+{
     public static final String MOD_ID = "gundamagedecreaser";
     private static final float DAMAGE_FACTOR = 0.575f;
+
     private static final String[] TARGETS = new String[]
             {
                     "gvclib",
@@ -27,39 +28,50 @@ public class Main {
 
 
 
+
+    //ModがInitializeを呼び出す前に発生するイベント。
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
-    { //ModがInitializeを呼び出す前に発生するイベント。
-        MinecraftForge.EVENT_BUS.register(this); //これでこのクラス内でForgeのイベントが動作するようになるらしい。
+    {
+        //これでこのクラス内でForgeのイベントが動作するようになるらしい。
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
 
 
+
+    //Entityが攻撃を受けたときのイベント。
     @SubscribeEvent
     public void onEntityHurt(LivingHurtEvent event)
     {
+        //現在の処理がサーバー側かつダメージを受けたエンティティが存在するか
         if (event.getEntity().world.isRemote) return;
-        // ダメージを受けたエンティティが存在するか
-        if (event.getEntityLiving() != null) {
-            // ダメージを与えたエンティティを取得
-            Entity attacker = event.getSource().getImmediateSource();
+        if (event.getEntityLiving() != null)
+        {
 
-            if (attacker != null)
+            final Entity LATTACKER = event.getSource().getImmediateSource();        // ダメージを与えたエンティティを取得
+
+            if (LATTACKER != null)
             {
-                String attacker_id = attacker.getClass().toString();
-                if (!isContainID(attacker_id, TARGETS)) return;      //ダメージ抑制の対象でないEntityならreturn
+                final String LATTACKER_ID = LATTACKER.getClass().toString();
 
-                // ダメージを(DAMAGE_FACTOR)%に減少させる
-                float reducedDamage = event.getAmount() * DAMAGE_FACTOR;
-                //もしMORE_DECREASE_TARGETSにも含まれるなら更に(DAMAGE_FACTOR)%に減少させる。
-                if (isContainID(attacker_id, MORE_DECREASE_TARGETS))
+                //ダメージ抑制の対象かどうか
+                if (isContainID(LATTACKER_ID, TARGETS))
                 {
-                    reducedDamage *= DAMAGE_FACTOR;
+
+                    float reduced_damage = event.getAmount() * DAMAGE_FACTOR;       // ダメージを(DAMAGE_FACTOR)%に減少させる
+
+                    //もしMORE_DECREASE_TARGETSにも含まれるなら更に(DAMAGE_FACTOR)%に減少させる。
+                    if (isContainID(LATTACKER_ID, MORE_DECREASE_TARGETS))
+                    {
+                        reduced_damage *= DAMAGE_FACTOR;
+                    }
+                    event.setAmount(reduced_damage);
                 }
-                event.setAmount(reducedDamage);
             }
         }
     }
+
 
 
 
